bac77c97b0ce3109357fd9fb05e9f6a62c102d9e
shivakumar323 <shivanavodaya007@gmail.com>
2021-05-06 11:53:46 +0530

    update for destination links

--- travello/views.py
+++ travello/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Destination
 # Create your views here.
 
@@ -8,3 +8,8 @@ def index(request):
     dests = Destination.objects.all()
 
     return render(request, 'index.html', {'dests': dests})
+
+
+def destinations(request):
+    dests = Destination.objects.all()
+    return render(request, 'destinations.html', {'dests': dests})
